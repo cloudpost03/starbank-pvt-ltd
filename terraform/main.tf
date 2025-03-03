3f61f95086889ec007aa1475721af3f2d75a0d7a
Pravin Kumar <84461089+cloudpost03@users.noreply.github.com>
2025-03-03 18:50:17 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,34 +1,28 @@
-resource "aws_instance" "Master3" {
-  ami           = "ami-00bb6a80f01f03502"
-  instance_type = "t2.medium"
-  key_name      = "MUMBAI"
-  tags = {
-    Name = "Machine -3"
-  }
+provider "aws" {
+  region = "ap-south-1" # Mumbai Region
 }
 
-resource "aws_instance" "K8s-Master" {
-  ami           = "ami-00bb6a80f01f03502"
-  instance_type = "t2.micro"
-  key_name      = "MUMBAI"
-  tags = {
-    Name = "Machine -1"
-  }
+# Define Variables
+variable "ami_id" {
+  default = "ami-00bb6a80f01f03502"
 }
 
-resource "aws_instance" "K8s-Slave1" {
-  ami           = "ami-00bb6a80f01f03502"
-  instance_type = "t2.micro"
-  key_name      = "MUMBAI"
-  tags = {
-    Name = "Machine -2"
-  }
+variable "instance_type_master" {
+  default = "t2.medium"
+}
+
+variable "instance_type_worker" {
+  default = "t2.medium"
+}
+
+variable "key_name" {
+  default = "MUMBAI"
 }
 
 # Security Group
-resource "aws_security_group" "devops_sg" {
+resource "aws_security_group" "default" {
   name        = "default"
-  description = "Allow SSH, HTTP, and monitoring ports"
+  description = "Allow required ports"
 
   ingress {
     from_port   = 22
@@ -70,5 +64,39 @@ resource "aws_security_group" "devops_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instances
+resource "aws_instance" "k8s_master" {
+  ami           = var.ami_id
+  instance_type = var.instance_type_worker
+  key_name      = var.key_name
+  security_groups = [aws_security_group.devops_sg.name]
+
+  tags = {
+    Name = "K8s-Master"
+  }
+}
+
+resource "aws_instance" "k8s_slave1" {
+  ami           = var.ami_id
+  instance_type = var.instance_type_worker
+  key_name      = var.key_name
+  security_groups = [aws_security_group.devops_sg.name]
+
+  tags = {
+    Name = "K8s-Slave1"
+  }
+}
+
+resource "aws_instance" "machine_3" {
+  ami           = var.ami_id
+  instance_type = var.instance_type_master
+  key_name      = var.key_name
+  security_groups = [aws_security_group.devops_sg.name]
+
+  tags = {
+    Name = "Machine-3"
   }
 }
