dceab05b2162c93987cf87f7a76d96d4a90b1e9d
Pravin Kumar <84461089+cloudpost03@users.noreply.github.com>
2025-03-03 19:04:01 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,7 @@
 provider "aws" {
-  region = "ap-south-1" # Mumbai Region
+  region = "ap-south-1"
 }
 
-# Define Variables
 variable "ami_id" {
   default = "ami-00bb6a80f01f03502"
 }
@@ -12,67 +11,23 @@ variable "instance_type_master" {
 }
 
 variable "instance_type_worker" {
-  default = "t2.medium"
+  default = "t2.micro"
 }
 
 variable "key_name" {
   default = "MUMBAI"
 }
 
-# Security Group
-resource "aws_security_group" "default" {
-  name        = "default"
-  description = "Allow required ports"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 9090
-    to_port     = 9090
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 6443
-    to_port     = 6443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+variable "security_group_id" {
+  default = "sg-0544446a68f71bb8c"
 }
 
 # EC2 Instances
 resource "aws_instance" "k8s_master" {
-  ami           = var.ami_id
-  instance_type = var.instance_type_worker
-  key_name      = var.key_name
-  security_groups = [aws_security_group.devops_sg.name]
+  ami                    = var.ami_id
+  instance_type          = var.instance_type_worker
+  key_name               = var.key_name
+  vpc_security_group_ids = [var.security_group_id]
 
   tags = {
     Name = "K8s-Master"
@@ -80,10 +35,10 @@ resource "aws_instance" "k8s_master" {
 }
 
 resource "aws_instance" "k8s_slave1" {
-  ami           = var.ami_id
-  instance_type = var.instance_type_worker
-  key_name      = var.key_name
-  security_groups = [aws_security_group.devops_sg.name]
+  ami                    = var.ami_id
+  instance_type          = var.instance_type_worker
+  key_name               = var.key_name
+  vpc_security_group_ids = [var.security_group_id]
 
   tags = {
     Name = "K8s-Slave1"
@@ -91,10 +46,10 @@ resource "aws_instance" "k8s_slave1" {
 }
 
 resource "aws_instance" "machine_3" {
-  ami           = var.ami_id
-  instance_type = var.instance_type_master
-  key_name      = var.key_name
-  security_groups = [aws_security_group.devops_sg.name]
+  ami                    = var.ami_id
+  instance_type          = var.instance_type_master
+  key_name               = var.key_name
+  vpc_security_group_ids = [var.security_group_id]
 
   tags = {
     Name = "Machine-3"
